d9ed52238a77356570a5efa20fa9560b3585a996
dansana <danielsana60@gmail.com>
2024-06-27 09:36:25 +0300

    first commit

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+message = "hello developers"
+myName ="daniel sana"
+age = 24
+total_marks =30
+totalMarks = 50
+_student ="micah"
+_2student ="fatuma"
+
+
+print(message)
+print(myName)
+print(age)
---
+++ myString.py
@@ -0,0 +1,5 @@
+myName ="daniel"
+mySchool ="modcom"
+myStack = 'MERN STACK'
+
+print("Hello there, my name is "+myName+" and my school is "+mySchool+" i love "+myStack)
